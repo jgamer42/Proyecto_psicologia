59902ae7cffe852bde8a3adc7620f9e666dbeb44
Jaime Andres Valencia <jgaimer42@gmail.com>
2021-08-15 12:54:59 -0500

    cambios en los personajes

--- machete.py
+++ machete.py
@@ -2,7 +2,7 @@ import os
 import json
 from dotenv import load_dotenv
 load_dotenv() 
-from src.components.bussines import personajes
+from src.components.bussines import personajes2
 def marcarPersonajes():
     base = os.getenv("PROJECT_PATH")+"/src/corpus"
     carpetas = os.listdir(base)
@@ -10,12 +10,16 @@ def marcarPersonajes():
         ruta = f"{base}/{carpeta}/json"
         archivos = os.listdir(ruta)
         for archivo in archivos:
-            print(f"{ruta}/{archivo}")
-            jsonCargado = open(f"{ruta}/{archivo}")
-            data = json.load(jsonCargado)
-            texto = data["aux"]+data["contenido"]
-            actores = personajes.etiquetar(texto)
-            data["actores"] = actores
-            jsonEscritor = open(f"{ruta}/{archivo}","a")
-            json.dump(data,jsonEscritor)
+            try:
+                print(f"{ruta}/{archivo}")
+                jsonCargado = open(f"{ruta}/{archivo}")
+                data = json.load(jsonCargado)
+                texto = data["aux"]+data["contenido"]
+                actores = personajes2.etiquetar(texto)
+                data["actores"] = actores
+                jsonEscritor = open(f"{ruta}/{archivo}","a")
+                json.dump(data,jsonEscritor)
+                print(actores)
+            except:
+                print(f"fallo {ruta}/{archivo}")
 marcarPersonajes()
